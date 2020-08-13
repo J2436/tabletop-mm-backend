e4339e0f59882e5d5d0544a1fc46d34255dd9a35
J2436 <wackejacky@gmail.com>
2020-08-13 13:42:20 -0700

    removed old token utils and am not using new TokenChecker middleware to check for user authentication

--- controllers/groups.js
+++ controllers/groups.js
@@ -2,72 +2,43 @@ const groupsRouter = require("express").Router();
 const Group = require("../models/group");
 const jwt = require("jsonwebtoken");
 const Player = require("../models/player");
-const { getTokenFrom } = require("../utils/token");
 
 groupsRouter.get("/", (req, res) => {
   Group.find({})
     .then((data) => {
       res.send(data);
     })
-    .catch((err) => {
-      console.log(err);
-    });
+    .catch((err) => {});
 });
 
 groupsRouter.get("/ownedGroups", (req, res) => {
-  const token = getTokenFrom(req);
-  const decodedToken = jwt.verify(token, process.env.SECRET);
-  if (decodedToken) {
-    Group.find({ owner: decodedToken.id })
-      .then((data) => {
-        res.send(data);
-      })
-      .catch((err) => res.send(err));
-  } else {
-    res.status(404).send();
-  }
+  Group.find({ owner: req.user.id })
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => res.status(401).send(err));
 });
 
 groupsRouter.get("/joinedGroups", (req, res) => {
-  const token = getTokenFrom(req);
-  const decodedToken = jwt.verify(token, process.env.SECRET);
-  if (decodedToken) {
-    Group.find({
-      owner: { $ne: decodedToken.id },
-      players: { $in: [decodedToken.id] },
-      meetingDateTime: { $gt: Math.floor(Date.now() / 1000) },
-    }).then((data) => res.send(data));
-  } else {
-    res.status(404).send();
-  }
+  Group.find({
+    owner: { $ne: req.user.id },
+    players: { $in: [req.user.id] },
+    meetingDateTime: { $gt: Math.floor(Date.now() / 1000) },
+  })
+    .then((data) => res.send(data))
+    .catch((err) => res.status(404).send(err));
 });
 
-groupsRouter.get("/unjoinedGroups", (req, res) => {
-  const token = getTokenFrom(req);
-  const decodedToken = jwt.verify(token, process.env.SECRET);
-  if (decodedToken) {
-    Group.find({
-      players: { $ne: [decodedToken.id] },
-      meetingDateTime: { $gt: Math.floor(Date.now() / 1000) },
-    })
-      .then((data) => {
-        res.send(data);
-      })
-      .catch((err) => res.status(401).send(err));
-  } else {
-    res.status(404).send();
-  }
+groupsRouter.get("/unjoinedGroups", async (req, res) => {
+  let unjoined = await Group.find({
+    players: { $nin: [req.user.id] },
+    meetingDateTime: { $gt: Math.floor(Date.now() / 1000) },
+  });
+  res.send(unjoined);
 });
 
 groupsRouter.post("/createGroup", async (req, res) => {
-  const token = getTokenFrom(req);
-  const decodedToken = jwt.verify(token, process.env.SECRET);
-  if (!token || !decodedToken.id) {
-    return res.status(401).json({ error: "token missing or invalid" });
-  }
-
-  const user = await Player.findById(decodedToken.id);
-
+  const user = await Player.findById(req.user.id);
   const newGroup = new Group({
     ...req.body,
     owner: user._id,
@@ -76,8 +47,26 @@ groupsRouter.post("/createGroup", async (req, res) => {
 
   await newGroup.save();
   user.groups.concat(newGroup._id);
-
   res.send(newGroup);
+});
+
+groupsRouter.post("/joinGroup", async (req, res) => {
+  const group = await Group.findById(req.body.groupID);
+
+  if (group) {
+    let updatedGroup = await Group.findByIdAndUpdate(
+      req.body.groupID,
+      { $push: { players: req.user.id } },
+      { useFindAndModify: false }
+    );
+    res.send(updatedGroup);
+  } else {
+    res.status(404).send();
+  }
+});
+
+groupsRouter.get("/leaveGroup", (req, res) => {
+  res.send(req.user);
 });
 
 module.exports = groupsRouter;
