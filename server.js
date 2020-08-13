5e029d68decebf40478090a78c5a5670ae10156d
J2436 <wackejacky@gmail.com>
2020-08-13 13:42:56 -0700

    using new TokenChecker middleware

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const cors = require("cors");
 const groupsRouter = require("./controllers/groups");
 const playersRouter = require("./controllers/players");
 const loginRouter = require("./controllers/login");
+const TokenChecker = require("./middleware/TokenChecker");
 mongoose
   .connect(config.MONGODB_URI, {
     useNewUrlParser: true,
@@ -20,8 +21,8 @@ mongoose
 app.use(express.json());
 app.use(cors({ credentials: true, origin: true }));
 app.use(morgan("tiny"));
-
 app.use("/login", loginRouter);
+app.use(TokenChecker);
 app.use("/players", playersRouter);
 app.use("/groups", groupsRouter);
 app.listen(config.PORT);
